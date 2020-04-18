7bf9392e7360153fb0da98fc337cf9e7efab784c
mdolmaci <muammer94@gmail.com>
2020-04-18 12:57:29 +0300

    npm installed

---
+++ first-react/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm React App</h1>
+        <p>It works!</p>
+        <Person />
+      </div>
+    );
+
+    //return React.createElement('div', {ClassName: 'App'}, React.createElement('h1', null, 'Hi, My name is React!'));
+  }
+}
+
+export default App;
